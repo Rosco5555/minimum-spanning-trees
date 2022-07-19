233183414e0ff02fd40d6b66c93a7b494c58028f
rosco555 <rosswburnett313@gmail.com>
2022-07-19 20:56:20 +0100

    Adding implementation of prim's algorithm
    Generates random vertices, and uses prim's algorithm to draw their
    minimum spanning tree.

--- MST.js
+++ MST.js
@@ -1,23 +1,35 @@
 // Canvas setup
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-canvas.width = window.innerWidth
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Variables
-vertices = []
-vertex_radius = 15
+var vertices = []
+var visited = []
+var unvisited = []
+var vertex_radius = 3
+var number_of_vertices = 1000
+var line_width = 1
 
 /**
  * First thing we want is when we click somewhere, is to draw a circle and add its position to
  * our list of vertices.
+ * 
+ * The algorithm is essentially...
+ * 
+ * keep track of unreached
  *  */
-function mouseClicked(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    vertices.push([x,y])
-    drawCircle(x, y)
+
+function setup() {
+    for (var i = 0; i < number_of_vertices; i++) {
+        let x = Math.floor(Math.random() * canvas.width)
+        let y =  Math.floor(Math.random() * canvas.height)
+        vertices.push([x,y])
+        unvisited.push([x,y])
+    }
 }
+
 
 function drawCircle(x , y) {
     ctx.beginPath();
@@ -35,5 +47,46 @@ function draw() {
     drawVertices();
 }
 
+function distance(a, b) {
+    var c = a[0]-b[0]
+    var d = (b[0]-b[1])
+    return Math.sqrt( c*c + d*d );
+}
 
-canvas.addEventListener("click", mouseClicked);
+
+function prims() {
+    visited.push(vertices[0]);
+    unvisited.splice(0, 1)
+    var u_index = 0;
+    var r_index = 0;
+
+    while (unvisited.length > 0) { 
+        var min_total_dist = 100000;
+        for (var i = 0; i < visited.length; i++) {
+            for (var j = 0; j < unvisited.length; j++) {
+                if (distance(visited[i], unvisited[j]) < min_total_dist) {
+                    min_total_dist = distance(visited[i], unvisited[j]);
+                    u_index = j;
+                    r_index = i;
+                }
+            }
+        }
+        // set line stroke and line width
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = line_width;
+    
+        // draw a red line
+        ctx.beginPath();
+        ctx.moveTo(visited[r_index][0], visited[r_index][1]);
+        ctx.lineTo(unvisited[u_index][0], unvisited[u_index][1]);
+        ctx.stroke();
+
+        visited.push(unvisited[u_index]);
+        unvisited.splice(u_index, 1);
+
+    }
+}
+
+setup()
+draw()
+prims()
